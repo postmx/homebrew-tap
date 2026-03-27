4a309bdbf03e9e0e5b3c073616178b80b5fb0740
Hariharan <hello@hariharanm.com>
2026-03-28 02:34:25 +0530

    Update postmx 0.1.3

--- Formula/postmx.rb
+++ Formula/postmx.rb
@@ -1,8 +1,8 @@
 class Postmx < Formula
   desc "CLI for the PostMX email testing API"
   homepage "https://github.com/postmx/cli"
-  url "https://registry.npmjs.org/postmx-cli/-/postmx-cli-0.1.2.tgz"
-  sha256 "a29721f826620a038b7a9ce9374748533666c0094a5f82153dbec4b51bcf16c0"
+  url "https://registry.npmjs.org/postmx-cli/-/postmx-cli-0.1.3.tgz"
+  sha256 "3909ac86584f20ae95a6d44ed1990a3e3d0a61fe18252e208167caab9569fe52"
   license "MIT"
 
   depends_on "node"
